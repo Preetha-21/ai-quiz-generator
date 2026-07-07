3dba72bc4ed79609c0f053d33b2bc9b19598d4ef
preetha <preethakaliappan289@gmail.com>
2026-07-07 22:29:28 +0530

    Connect backend to TiDB Cloud

--- backend/database.py
+++ backend/database.py
@@ -5,14 +5,16 @@ from mysql.connector import Error
 def get_connection():
     try:
         connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="Preetha@21",
-            database="quiz_generator_agent"
+            host="gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com",
+            port=4000,
+            user="2MXSmWTbKcPNxc3.root",
+            password="CyFF4IAY371cPYTL",
+            database="quiz_generator_agent",
+            ssl_ca="C:/Users/preet/Downloads/isrgrootx1.pem"
         )
 
         if connection.is_connected():
-            print("Connected to MySQL successfully.")
+            print("Connected to TiDB successfully.")
             return connection
 
     except Error as e:
